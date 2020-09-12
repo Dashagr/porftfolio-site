a2018224e6ad9c66524a6333d916d4d217a770a3
unknown <dasha.g1917@gmail.com>
2020-09-12 17:38:21 +0300

    Adding new works into portfolio

--- js/script.js
+++ js/script.js
@@ -1,19 +1,27 @@
-$(".mobile-tab").hide();
+// toggle menu
+$('.mobile-tab').hide();
 
-$("#burg").on("click", function () {
-  $(".mobile-tab").slideToggle(500);
+$('#burg').on('click', function () {
+  $('.mobile-tab').slideToggle(500);
 });
 
+//easy scrolling effect
 var headerHeight = $(".navigation").outerHeight();
 
-$(".scroll").click(function (e) {
-  var linkHref = $(this).attr("href");
-  $("html, body").animate(
-    {
+$('.scroll').click(function (e) {
+  var linkHref = $(this).attr('href');
+  $('html, body').animate({
       scrollTop: $(linkHref).offset().top - headerHeight,
     },
     1000
   );
 
   e.preventDefault();
+});
+
+//showing more portfolio works
+
+$('.more-grid').hide();
+$('.more').on('click', function () {
+  $('.more-grid').slideToggle(500);
 });
